9626e5be342e5dd65444b6bce6e518dfba5bb3a0
Kevin <kevin_mcp@outlook.com>
2022-10-10 18:14:56 -0300

    Adicionando novas imagens do profile

--- src/assets/Profile.tsx
+++ src/assets/Profile.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import profile from "../../public/profile.jpg";
+import profile from "../../public/profile.png";
 
 export function Profile() {
   return <Image src={profile} alt="Profile" />;
