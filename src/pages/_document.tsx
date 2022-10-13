d70dce5aa275b5dfd14eb89567c50ac36ac1eb81
Kevin <kevin_mcp@outlook.com>
2022-10-13 12:20:44 -0300

    styles: tema oscuro por default

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,6 @@
-import { ColorModeScript, theme } from "@chakra-ui/react";
+import { ColorModeScript } from "@chakra-ui/react";
 import Document, { Head, Html, Main, NextScript } from "next/document";
+import { theme } from "../styles/theme";
 
 export default class MyDocument extends Document {
   render() {
